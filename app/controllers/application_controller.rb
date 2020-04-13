e441b998e0228bb44a458d92976e5d3f02ede72b
Balbina <balbinasantana@gmail.com>
2020-04-13 12:43:25 -0500

    pprobzz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
         token = request.headers['Authorization']
         if token.present?
             begin
-            jwt_payload = JWT.decode(token, 11f06107c78363628e14531d7431b89c).first
+            jwt_payload = JWT.decode(token, "d9c2fb4fbd8dece91adf53604d0662376112aff81c36f911931d57e1e2880ea12e2733f770a188117c23673e6dff00bee413225d42360d130c4512995e77b71b").first
     
             @current_user_id = jwt_payload['id']
             rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   def generate_jwt
     JWT.encode({ id: id,
                  exp: 60.days.from_now.to_i },
-                 11f06107c78363628e14531d7431b89c)
+                 "d9c2fb4fbd8dece91adf53604d0662376112aff81c36f911931d57e1e2880ea12e2733f770a188117c23673e6dff00bee413225d42360d130c4512995e77b71b")
   end
 end
